cbc20eeeaa979aee4ac8fe6da9774fa5ee170711
fares <fares.droubi@gmail.com>
2016-01-20 00:33:02 +0100

    Simplifying the code and refactoring.

--- my-app/src/main/webapp/js/main.js
+++ my-app/src/main/webapp/js/main.js
@@ -1,54 +1,46 @@
 // Models
-window.Presentation = Backbone.Model.extend();
+window.Presentation = Backbone.Model.extend({
+    urlRoot: '../rest/presentations'
+});
 
 window.PresentationCollection = Backbone.Collection.extend({
-    model:Presentation,
     url:"../rest/presentations"
 });
 
 
 // Views
-window.Allpresentations = Backbone.View.extend({
+window.AllPresentations = Backbone.View.extend({
 
-    tagName:'div',
+    el: '#sidebar',
+    render:function () {
+        var that = this;
+        var presentationsCol = new PresentationCollection();
+        presentationsCol.fetch({
+            success: function (presentations) {
+                alert(JSON.stringify(presentations))
+                var dddd = presentations.toJSON()[0].presentations
+                var template = _.template($('#tpl-presentation-list-item').html(), {presentations: presentations.toJSON()[0].presentations});
+                $(that.$el).html(template);
+            }
 
-    template:_.template($('#tpl-presentation-list-item').html()),
-
-
-    initialize:function () {
-        this.model.bind("reset", this.render, this);
-
-    },
-
-    render:function (eventName) {
-        _.each(this.model.models, function (presentation) {
-            var ss = presentation
-            alert(JSON.stringify(ss))
-            $(this.el).html(this.template(presentation.toJSON()));
-        }, this);
-        return this;
+        })
     }
 
 });
 
 // Router
 var AppRouter = Backbone.Router.extend({
-
     routes:{
         "presentations": "presentations",
-    },
-
-    presentations: function () {
-        this.presentaionList = new PresentationCollection();
-        this.allpresentationView = new Allpresentations({model:this.presentaionList});
-        // should find a way to create render based on an event lisener.
-        var el = this.allpresentationView.render().el
-        this.presentaionList.fetch().then(function() {
-            $('#sidebar').html(el);
-        });
     }
+
 });
 
+var presentationList = new AllPresentations();
 
-var app = new AppRouter();
+var router = new AppRouter()
+router.on('route:presentations', function () {
+    presentationList.render();
+})
+
 Backbone.history.start();
